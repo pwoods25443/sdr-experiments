a1bf9bb29b882782e817ffc8ff8814816dcc8ad6
pwoods25443 <pwoods25443@gmail.com>
2020-08-19 09:44:32 -0400

    cli help doc update

--- ais/parse_ais.py
+++ ais/parse_ais.py
@@ -77,8 +77,8 @@ def format_messages(messages, fmt):
 @click.command(
     help="Utility for parsing a stream of AIS NMEA-encoded messages."
          "\n\n"
-         "INPUT should be a text stream with one NMEA message per line, and defaiults to stdin.  Use '-' to explicitly "
-         "use stdin"
+         "INPUT should be a text stream with one NMEA message per line, and defaiults to stdin.  "
+         "Use '-' to explicitly use stdin"
          "\n\n"
          "OUTPUT is a text stream of the parsed NMEA.  Use '-' for stdout "
          "\n\n"
@@ -88,13 +88,15 @@ def format_messages(messages, fmt):
          "\n\n"
          "outputs something like"
          "\n\n"
+         '{"tagblock_timestamp": 1577762601.537, "tagblock_station": "sdr-experiments", "tagblock_T": "2019-12-30 22.23.21", "nmea": "!AIVDM,1,1,,A,15NTES0P00J>tC4@@FOhMgvD0D0M,0*49\n", "id": 1, "repeat_indicator": 0, "mmsi": 367596940, "nav_status": 0, "rot_over_range": true, "rot": -731.386474609375, "sog": 0.0, "position_accuracy": 0, "x": -80.62191666666666, "y": 28.408531666666665, "cog": 11.800000190734863, "true_heading": 511, "timestamp": 10, "special_manoeuvre": 0, "spare": 0, "raim": false, "sync_state": 0, "slot_timeout": 5, "received_stations": 29}'
+         "\n\n"
          ""
          )
 @click.argument('input', type=click.File('r'), default='-')
 @click.argument('output', type=click.File('w'), default='-')
 @click.option('-f', '--format', 'fmt',
-              type=click.Choice(['CSV', 'JSON'], case_sensitive=False),
-              default='CSV',
+              type=click.Choice(['JSON'], case_sensitive=False),
+              default='JSON',
               help="output format.   JSON is newline delimited JSON, one object per message "
               )
 def parse(input, output, fmt):
